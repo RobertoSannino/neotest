2a055d9ffb96df29406cb6c24f0e98a79c7a0feb
RobertoSannino <rob.sannino@libero.it>
2020-11-27 16:11:08 +0100

    fix stack trace in api response

--- src/main/java/it/rob/test/neotest/exception/BadRequestException.java
+++ src/main/java/it/rob/test/neotest/exception/BadRequestException.java
@@ -3,7 +3,7 @@ package it.rob.test.neotest.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(code = HttpStatus.BAD_REQUEST, reason = "Bad Request")
+@ResponseStatus(code = HttpStatus.BAD_REQUEST)
 public class BadRequestException extends RuntimeException {
     public BadRequestException(String message) {
         super(message);
--- src/main/java/it/rob/test/neotest/exception/NotFoundException.java
+++ src/main/java/it/rob/test/neotest/exception/NotFoundException.java
@@ -3,7 +3,7 @@ package it.rob.test.neotest.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(code = HttpStatus.NOT_FOUND, reason = "Resource not found")
+@ResponseStatus(code = HttpStatus.NOT_FOUND)
 public class NotFoundException extends RuntimeException {
 
     public NotFoundException(String message) {
